4418ca0f245956c64c31449a3efcdb31e827f344
MiLu <michelle.lundak@yahoo.com>
2021-02-03 16:57:25 +0100

    signin page

---
+++ frontend/src/components/SignIn/DataTitleComp/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState} from 'react';
+import {Link} from 'react-router-dom';
+//img
+
+//css
+import {DataTitleCompStyle} from '../DataTitleComp/styles';
+
+const DataTitleComp =()=>{
+
+
+    return(
+        <DataTitleCompStyle>Sign In</DataTitleCompStyle>
+    )
+}
+export default DataTitleComp;
---
+++ frontend/src/components/SignIn/DataTitleComp/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const DataTitleCompStyle = styled.section`
+
+    display: flex;
+    font-size: 40px;
+    padding-bottom: 20px;
+    font-weight: lighter;
+    color: white;
+`
