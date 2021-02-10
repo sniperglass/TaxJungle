9f8e227201052345a7e1f84f7cce46499cf11093
M4V88 <marco_volken@bluewin.ch>
2021-02-10 21:40:24 +0100

    Resolve mixed content warnings and max body size issue

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -44,6 +44,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://0.0.0.0:8000',
 ]
 
+# Fixes mixed-content and too-many redirects warning
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
